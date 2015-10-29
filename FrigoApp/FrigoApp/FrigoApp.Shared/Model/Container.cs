913c575abc8759914ec41103bc2a24cb9dee04a4
Zamerine <ptitlelex@hotmail.com>
2015-10-29 04:56:36 +0100

    container debut

---
+++ FrigoApp/FrigoApp/FrigoApp.Shared/Model/Container.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace FrigoApp.Model
+{
+    class Container
+    {
+        public String id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public String Name { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public String Type { get; set; }
+
+        [JsonProperty(PropertyName = "proprio")]
+        public String Proprio { get; set; }
+
+        public Container()
+        {
+
+        }
+
+        public Container(String name, String type, String proprio)
+        {
+            Name = name;
+            Type = type;
+            Proprio = proprio;
+        }
+
+        public override String ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FrigoApp/FrigoApp/FrigoApp.Shared/ViewModel/ContainerViewModel.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using FrigoApp.Model;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace FrigoApp.ViewModel
+{
+    class ContainerViewModel :ViewModelBase
+    {
+        private INavigationService _navigationService;
+
+        public ObservableCollection<Container> containers { get; private set; }
+
+        private IMobileServiceTable<Container> containerTable = App.FappClient.GetTable<Container>();
+
+        private String name;
+
+        public String Name
+        {
+            get { return name;  }
+            set {
+                name = value;
+                RaisePropertyChanged("name");
+            }
+        }
+
+        private String type;
+
+        public String Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                RaisePropertyChanged("type");
+            }
+        }
+
+        private String proprio;
+
+        public String Proprio
+        {
+            get { return proprio; }
+            set
+            {
+                proprio = value;
+                RaisePropertyChanged("proprio");
+            }
+        }
+
+        public ContainerViewModel (INavigationService navigationService = null)
+        {
+            _navigationService = navigationService;
+        }
+
+    }
+}
